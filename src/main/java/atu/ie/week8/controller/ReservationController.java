f45a76bbeb24ccb4657eda42960f41aba7f1bb4e
Beatrice Papi <G00461762@atu.ie>
2026-03-16 00:54:59 +0000

    ReservationController in service controller

---
+++ src/main/java/atu/ie/week8/controller/ReservationController.java
@@ -0,0 +1,47 @@
+package atu.ie.week8.controller;
+
+import atu.ie.week8.model.Reservation;
+import atu.ie.week8.service.ReservationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    // POST /reservations
+    @PostMapping
+    public ResponseEntity<Reservation> createReservation(@Valid @RequestBody Reservation reservation) {
+        Reservation saved = reservationService.createReservation(reservation);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+
+    // GET /reservations
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getAllReservations() {
+        return ResponseEntity.ok(reservationService.getAllReservations());
+    }
+
+    // GET /reservations/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.getReservationById(id));
+    }
+
+    // Part B endpoint
+    // GET /reservations/equipment/{tag}
+    @GetMapping("/equipment/{tag}")
+    public ResponseEntity<List<Reservation>> getByEquipmentTag(@PathVariable String tag) {
+        return ResponseEntity.ok(reservationService.getByEquipmentTag(tag));
+    }
+}
