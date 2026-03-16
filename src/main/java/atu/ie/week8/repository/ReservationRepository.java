8122b41b7a9f07c654cd5eaf959a2e13db93b187
Beatrice Papi <G00461762@atu.ie>
2026-03-16 00:30:41 +0000

    ReservationRepository in reservation package

---
+++ src/main/java/atu/ie/week8/repository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package atu.ie.week8.repository;
+
+import atu.ie.week8.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    // Custom query method (Part B requirement)
+    List<Reservation> findByEquipmentTag(String equipmentTag);
+
+}
